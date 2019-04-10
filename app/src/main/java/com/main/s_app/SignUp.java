d91b3c115f86bf5f0135630739211d1e67c534b8
Timur Celik <timur.celik@hs-augsburg.de>
2019-04-10 13:54:45 +0200

    login first sketch

--- app/src/main/java/com/main/s_app/SignUp.java
+++ app/src/main/java/com/main/s_app/SignUp.java
@@ -3,11 +3,11 @@ package com.main.s_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 }
