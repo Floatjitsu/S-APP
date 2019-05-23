165ae61a33f51e98cdfd2517145432123ddf2754
Yonkt <yonkt77@gmail.com>
2019-05-23 14:30:08 +0200

    adding the top bar on the news part

--- app/src/main/java/com/main/s_app/News.java
+++ app/src/main/java/com/main/s_app/News.java
@@ -4,15 +4,33 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class News extends Fragment {
 
+    Toolbar mToolbar;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View myView = inflater.inflate(R.layout.fragment_news, container, false);
+        mToolbar = myView.findViewById(R.id.toolbar_news);
+        if(getActivity() != null) { //Parent Activity is available
+            ((AppCompatActivity) getActivity()).setSupportActionBar(mToolbar); //Set Toolbar
+        }
+        setHasOptionsMenu(true);
+        return myView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.news_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
