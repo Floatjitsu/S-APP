38d5ad32d460a7e6e14dd1456463b11f9cda2beb
Timur Celik <timur.celik@hs-augsburg.de>
2019-05-23 19:01:48 +0200

    basic comment activity for text posts

--- app/src/main/java/com/main/s_app/PostComments.java
+++ app/src/main/java/com/main/s_app/PostComments.java
@@ -1,21 +1,24 @@
 package com.main.s_app;
 
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.FrameLayout;
+import android.util.TypedValue;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.main.s_app.com.main.s_app.firebase.TextPost;
 
+import java.util.Objects;
+
 public class PostComments extends AppCompatActivity {
 
+    Bundle mBundle;
+
     Toolbar mToolbar;
-    ConstraintLayout mConstraintLayout;
+    LinearLayout mLinearLayout;
+    TextView mPostTitle, mPostPostedBy, mPostDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,37 @@ public class PostComments extends AppCompatActivity {
         mToolbar = findViewById(R.id.toolbar_post_comments);
         setUpToolbar();
 
-        mConstraintLayout = findViewById(R.id.post_comments_placeholder);
+        mPostDate = findViewById(R.id.post_comments_date);
+        mPostTitle = findViewById(R.id.post_comments_title);
+        mPostPostedBy = findViewById(R.id.post_comments_posted_by);
 
-        Bundle bundle = getIntent().getExtras();
-       /* LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
-        View childLayout = inflater.inflate(R.layout.text_post_item, null);
-        mFrameLayout.addView(childLayout); */
+        mBundle = getIntent().getExtras();
+
+        mLinearLayout = findViewById(R.id.post_comments_placeholder);
+
+        initPost();
+    }
+
+    private void initPost() {
+        //Post title, date and posted by are always part of a post
+        mPostDate.setText(mBundle.getString(Constants.KEY_POST_DATE));
+        mPostPostedBy.setText(Objects.requireNonNull(mBundle.getString(Constants.KEY_POST_POSTED_BY)).replace("Posted by", ""));
+        mPostTitle.setText(mBundle.getString(Constants.KEY_POST_DATE));
+        //Check which kind of posts got pressed in the forum
+        switch (Objects.requireNonNull(mBundle.getString(Constants.KEY_POST_KIND))) {
+            case TextPost.POST_KIND:
+                initTextPost();
+                break;
+        }
+        //TODO: Add other cases for image post and link post
+    }
+
+    private void initTextPost() {
+        TextView textPostContent = new TextView(this);
+        textPostContent.setTextColor(ContextCompat.getColor(this, R.color.textColor));
+        textPostContent.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+        textPostContent.setText(mBundle.getString(Constants.KEY_TEXT_POST_CONTENT));
+        mLinearLayout.addView(textPostContent);
     }
 
     private void setUpToolbar() {
